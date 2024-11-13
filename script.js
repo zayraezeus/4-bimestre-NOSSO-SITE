7df848f460cb0e0510136e4df4b21e10c444a15e
zayraezeus <00001073278517sp@al.educacao.sp.gov.br>
2024-11-13 11:02:52 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Função para simular o envio de mensagem
+function enviarMensagem() {
+    alert("Mensagem enviada com sucesso!");
+    // Evita que o formulário seja realmente enviado (no exemplo)
+    event.preventDefault();
+  }
+  
